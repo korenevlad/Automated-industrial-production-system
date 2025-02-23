e11c1ff977cfc636d8b7849086d7fc3897bdae4c
Владислав Коренев <141056591+korenevlad@users.noreply.github.com>
2025-02-23 14:55:21 +0300

    add molding consumer

--- Backend/src/KafkaConsumer/KafkaConsumerService.cs
+++ Backend/src/KafkaConsumer/KafkaConsumerService.cs
@@ -1,30 +1,45 @@
 ﻿using Confluent.Kafka;
 using Microsoft.Extensions.Hosting;
 
-namespace KafkaConsume;
+namespace KafkaConsumer;
 public class KafkaConsumerService : BackgroundService
 {
-    private const string Topic = "mixing_components_producer";
+    private const string MixingComponentsProducerTopic = "mixing_components_producer";
+    private const string MoldingAndInitialExposureProducerTopic = "molding_and_initial_exposure_producer";
     private const string GroupId = "backend-group";
     private const string BootstrapServers = "kafka:9093";
-
+    private readonly ConsumerConfig _consumerConfig = new()
+    {
+        BootstrapServers = BootstrapServers,
+        GroupId = GroupId,
+        AutoOffsetReset = AutoOffsetReset.Earliest
+    };
+    
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        return Task.Run(() => ConsumeMessages(stoppingToken), stoppingToken);
+        return Task.WhenAll(
+            Task.Run(() => MixingComponentsProducerTopicConsumeMessages(stoppingToken), stoppingToken),
+            Task.Run(() => MoldingAndInitialExposureProducerConsumeMessages(stoppingToken), stoppingToken)
+        );
     }
-
-    private void ConsumeMessages(CancellationToken stoppingToken)
+    
+    private void MixingComponentsProducerTopicConsumeMessages(CancellationToken stoppingToken)
     {
-        var config = new ConsumerConfig
-        {
-            BootstrapServers = BootstrapServers,
-            GroupId = GroupId,
-            AutoOffsetReset = AutoOffsetReset.Earliest
-        };
-
-        using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
-        consumer.Subscribe(Topic);
-
+        ConsumeMessages(stoppingToken, MixingComponentsProducerTopic, 
+            "Полученные параметры смешивания компонентов", 
+            "Ошибка получения параметров смешивания компонентов" );
+    }
+    private void MoldingAndInitialExposureProducerConsumeMessages(CancellationToken stoppingToken)
+    {
+        ConsumeMessages(stoppingToken, MoldingAndInitialExposureProducerTopic, 
+            "Полученные параметры формования и первичной выдержки", 
+            "Ошибка получения параметров формования и первичной выдержки" );
+    }
+    
+    private void ConsumeMessages(CancellationToken stoppingToken, string topic, string successMessage, string errorMessage)
+    {
+        using var consumer = new ConsumerBuilder<Ignore, string>(_consumerConfig).Build();
+        consumer.Subscribe(topic);
         try
         {
             while (!stoppingToken.IsCancellationRequested)
@@ -32,11 +47,11 @@ public class KafkaConsumerService : BackgroundService
                 try
                 {
                     var consumeResult = consumer.Consume(stoppingToken);
-                    Console.WriteLine($"Получено сообщение: {consumeResult.Value}");
+                    Console.WriteLine($"{successMessage}: {consumeResult.Value}");
                 }
                 catch (ConsumeException e)
                 {
-                    Console.WriteLine($"Ошибка при получении сообщения: {e.Error.Reason}");
+                    Console.WriteLine($"{errorMessage}: {e.Error.Reason}");
                 }
             }
         }
--- Backend/src/KafkaConsumer/Program.cs
+++ Backend/src/KafkaConsumer/Program.cs
@@ -1,4 +1,4 @@
-﻿using KafkaConsume;
+﻿using KafkaConsumer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
