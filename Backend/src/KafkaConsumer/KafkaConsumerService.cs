bf64234f97be65b88ab5e59d942ba2c2268da47a
Владислав Коренев <141056591+korenevlad@users.noreply.github.com>
2025-02-23 12:22:44 +0300

    Add consumer

---
+++ Backend/src/KafkaConsumer/KafkaConsumerService.cs
@@ -0,0 +1,48 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+
+namespace KafkaConsume;
+public class KafkaConsumerService : BackgroundService
+{
+    private const string Topic = "mixing_components_producer";
+    private const string GroupId = "backend-group";
+    private const string BootstrapServers = "kafka:9093";
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        return Task.Run(() => ConsumeMessages(stoppingToken), stoppingToken);
+    }
+
+    private void ConsumeMessages(CancellationToken stoppingToken)
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = BootstrapServers,
+            GroupId = GroupId,
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+        consumer.Subscribe(Topic);
+
+        try
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var consumeResult = consumer.Consume(stoppingToken);
+                    Console.WriteLine($"Получено сообщение: {consumeResult.Value}");
+                }
+                catch (ConsumeException e)
+                {
+                    Console.WriteLine($"Ошибка при получении сообщения: {e.Error.Reason}");
+                }
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            consumer.Close();
+        }
+    }
+}
---
+++ Backend/src/KafkaConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using KafkaConsume;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddHostedService<KafkaConsumerService>();
+    })
+    .Build();
+
+await host.RunAsync();
