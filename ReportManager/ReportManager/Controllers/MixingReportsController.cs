b739191cc96c666cd58032d57ccbd9e7f5f8d4fa
Владислав Коренев <141056591+korenevlad@users.noreply.github.com>
2025-03-06 01:09:59 +0300

    .

---
+++ ReportManager/ReportManager/Controllers/MixingReportsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReportManager.DataAccess.Repository;
+
+namespace ReportManager.Controllers;
+
+public class MixingReportsController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+    public MixingReportsController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    public IActionResult Parameters(Guid id)
+    {
+        var parametersMixing = _unitOfWork.ParametersMixingProcessRepository
+            .GetAll().Where(p => p.mixing_process_id == id).ToList();
+        return View(parametersMixing);
+    }
+    public IActionResult Errors(Guid id)
+    {
+        var parametersMixing = _unitOfWork.ParametersMixingProcessRepository
+            .GetAll().Where(p => p.mixing_process_id == id 
+                                 && (p.temperature_mixture_is_normal == false || p.mixing_speed_is_normal == false)).ToList();
+        return View(parametersMixing);
+    }
+}
---
+++ ReportManager/ReportManager/Controllers/ReportsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReportManager.DataAccess.Repository;
+
+namespace ReportManager.Controllers;
+public class ReportsController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+    public ReportsController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    public IActionResult Mixing(Guid id)
+    {
+        var mixingProcess = _unitOfWork.MixingProcessRepository.GetAll()
+            .FirstOrDefault(p => p.technological_process_id == id);
+        return View(mixingProcess.mixing_process_id);
+    }
+    public IActionResult Molding(Guid id)
+    {
+        var moldingProcess = _unitOfWork.MoldingAndInitialExposureProcessRepository.GetAll()
+            .FirstOrDefault(p => p.technological_process_id == id);
+        return View(moldingProcess.molding_and_initial_exposure_process_id);
+    }
+    public IActionResult Cutting(Guid id)
+    {
+        var cuttingProcess = _unitOfWork.CuttingArrayProcessRepository.GetAll()
+            .FirstOrDefault(p => p.technological_process_id == id);
+        return View(cuttingProcess.cutting_array_process_id);
+    }
+    public IActionResult Autoclaving(Guid id)
+    {
+        var autoclavingProcess = _unitOfWork.AutoclavingProcessRepository.GetAll()
+            .FirstOrDefault(p => p.technological_process_id == id);
+        return View(autoclavingProcess.autoclaving_process_id);
+    }
+}
